f40408b6c4504a73098c7a58f4789989e739d427
vletrmx21 <kult404@gmail.com>
2017-01-16 00:16:42 +0300

    dsfsdfsfdsdffsd

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+
+	def update_without_password(params, *options)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+  
 end
